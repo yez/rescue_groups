1772c81e4760c6b6fd6c86260c08292aca28e101
Jake Yesbeck <jake@apartmentlist.com>
2015-05-09 00:28:24 -0700

    Skeleton for search
    AnimalSearch class to wrap up Filters and AnimalFields nicely and
      export their configuration as a json body
    AnimalField class of defined fields that API returns/filters on
    Filter class to abstract different kinds of assertions against
      the AnimalField(s)

---
+++ search/animal_search.rb
@@ -0,0 +1,47 @@
+require_relative './filter'
+require_relative './animal_field'
+
+class AnimalSearch
+  attr_accessor :start, :limit, :sort, :order,
+                :calc_found_rows, :fields, :filters
+
+  def initialize(start = 0, limit = 10, sort = :animalID, order = :asc, fields = [])
+    @start           = start
+    @limit           = limit
+    @sort            = sort
+    @order           = order
+    @calc_found_rows = 'Yes'
+    @fields          = fields || AnimalField.all
+    @filters         = []
+  end
+
+  def add_filter(name, assertion, equals)
+    @filters << Filter.new(filter_name(name), assertion, filter_value(equals))
+  end
+
+  def filter_name(name)
+    AnimalField::FIELDS[name]
+  end
+
+  def filter_value(value)
+    {
+      available: :Available,
+    }[value.to_sym] || value
+  end
+
+  def as_json(*args)
+    {
+      resultStart:   start,
+      resultLimit:   limit,
+      resultSort:    sort,
+      resultOrder:   order,
+      calcFoundRows: calc_found_rows,
+      filters:       filters.map(&:as_json),
+      fields:        fields
+    }
+  end
+
+  def to_json(*)
+    JSON(as_json)
+  end
+end
---
+++ search/filter.rb
@@ -0,0 +1,24 @@
+class Filter
+  attr_accessor :name, :operation, :criteria
+
+  OPERATIONS = %i[equal notequal
+                  lessthan lessthanorequal
+                  greaterthan greaterthanorequal
+                  contains notcontain
+                  blank notblank
+                  radius]
+
+  def initialize(name, operation, criteria)
+    @name      = name
+    @operation = OPERATIONS.find { |o| o == operation.to_sym }
+    @criteria  = criteria
+  end
+
+  def as_json(*args)
+    {
+      fieldName: name,
+      operation: operation,
+      criteria: criteria
+    }
+  end
+end
