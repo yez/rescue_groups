adb40da2264b096cde92861b95a3e0d0cb33b9e1
Jake Yesbeck <jake@apartmentlist.com>
2015-05-26 00:07:48 -0700

    Placeholder spec for response

---
+++ spec/lib/response_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../spec_helper'
+require_relative '../../lib/response'
+
+module RescueGroups
+  describe Response do
+    context 'a successful response' do
+    end
+
+    context 'an errorful response' do
+    end
+  end
+end
