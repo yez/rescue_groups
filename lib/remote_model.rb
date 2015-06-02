2dc28114906e2376b8f0c641e889d76e1e0bf40c
Jake Yesbeck <jake@apartmentlist.com>
2015-06-01 21:20:48 -0700

    Update RemoteModel to allow initialization with nil

--- lib/remote_model.rb
+++ lib/remote_model.rb
@@ -2,8 +2,8 @@ module RescueGroups
   module RemoteModel
     def self.included(base)
       base.class_eval do
-        def initialize(attribute_hash)
-          attribute_hash.each do |key, value|
+        def initialize(attribute_hash = {})
+          (attribute_hash || {}).each do |key, value|
             mapped_method = "#{ self.class.object_fields::FIELDS.key(key.to_sym) }="
             next unless self.respond_to?(mapped_method)
             self.send(mapped_method, value)
