57319be4a7fbcbc4692cc2ecd887daad53f1f68e
Jake Yesbeck <jake@apartmentlist.com>
2015-06-24 00:01:05 -0700

    Relationable documentation and api update

--- lib/relationable.rb
+++ lib/relationable.rb
@@ -1,10 +1,30 @@
 module RescueGroups
   module Relationable
+    # This method is called when the Relationable Module is included
+    #   in a class.
     def self.included(base)
       base.extend(ClassMethods)
     end
 
     module ClassMethods
+      # method: belongs_to
+      # purpose: define methods that denote a relationship
+      #            between the class including this module
+      #            and another.
+      #          When included, it defines methods: relationship_id
+      #                                             relationship_id=
+      #                                             relationship
+      #                                             relationship=
+      # example:
+      #    class Foo
+      #      include Relationable
+      #
+      #      belongs_to :bar
+      #    end
+      #
+      # param: relationship - <Symbol> - name of the class that a relationship
+      #          is intended for
+      # return: nil
       def belongs_to(relationship)
         define_method relationship do
           model = instance_variable_get(:"@#{ relationship }")
@@ -13,7 +33,7 @@ module RescueGroups
           relationship_id = self.send(:"#{ relationship }_id")
 
           unless relationship_id.nil?
-            klass = self.class.constantize(relationship)
+            klass = self.class.send(:constantize, relationship)
 
             self.send(:"#{ relationship }=", klass.find(relationship_id))
           end
@@ -31,7 +51,45 @@ module RescueGroups
           instance_variable_set(:"@#{ relationship }_id", value)
         end
       end
+      # method: has_many
+      # purpose: define methods that denote a relationship
+      #            between the class including this module
+      #            and another.
+      #          When included, it defines methods: relationship
+      #                                             relationship=
+      # example:
+      #    class Foo
+      #      include Relationable
+      #
+      #      has_many :bars
+      #    end
+      #
+      # param: relationship - <Symbol> - name of the class that a relationship
+      #          is intended for
+      # return: nil
+      def has_many(relationship)
+        define_method relationship do
+          temp = instance_variable_get(:"@#{ relationship }")
 
+          return temp unless temp.nil? || temp.empty?
+
+          klass = self.class.send(:constantize, relationship)
+          foreign_key = "#{ self.class.to_s.split('::').last.downcase }_id"
+          klass.where(foreign_key.to_sym => @id)
+        end
+
+        define_method :"#{ relationship }=" do |value|
+          instance_variable_set(:"@#{ relationship }", value)
+        end
+      end
+
+      private
+
+      # method: constantize
+      # purpose: given a symbol, convert it into a class name and
+      #            return the matching constant
+      # param: relationship - <symbol> - name of the constant
+      # return: constant that is defined or it raises an error
       def constantize(relationship)
         klass = ''
 
@@ -48,22 +106,6 @@ module RescueGroups
           else
             Object.const_get("RescueGroups::#{ klass[0..-2] }")
           end
-        end
-      end
-
-      def has_many(relationship)
-        define_method relationship do
-          temp = instance_variable_get(:"@#{ relationship }")
-
-          return temp unless temp.nil? || temp.empty?
-
-          klass = self.class.constantize(relationship)
-          foreign_key = "#{ self.class.to_s.split('::').last.downcase }_id"
-          klass.where(foreign_key.to_sym => @id)
-        end
-
-        define_method :"#{ relationship }=" do |value|
-          instance_variable_set(:"@#{ relationship }", value)
         end
       end
     end
