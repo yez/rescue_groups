69471df143fba8fdf020cb62542d399e38be4048
Jake Yesbeck <jake@apartmentlist.com>
2015-06-25 21:19:22 -0700

    Documentation for config classes

--- config/config.rb
+++ config/config.rb
@@ -3,10 +3,23 @@ module RescueGroups
     attr_accessor :apikey
   end
 
+  # method: config
+  # purpose: class method to instantiate or return an already
+  #           instantiated Config class
+  # param: none
+  # return: <Config> - instance of the config class
   def self.config
     @config ||= Config.new
   end
 
+  # method: configuration
+  # purpose: yield to a block of configuration parameters
+  #          example:
+  #            RescueGroups.configuration do |config|
+  #              config.api_key = 'anything'
+  #            end
+  # param: none
+  # return: none
   def self.configuration
     yield config
   end
--- config/initializer.rb
+++ config/initializer.rb
@@ -1,8 +1,10 @@
 require 'json'
 require 'httparty'
+# This must be required before configuration can happen
 require_relative './config'
 
 RescueGroups.configuration do |config|
+  # Your API key should be set in an ENV variable called API_KEY
   config.apikey = ENV['API_KEY'] || ''
 end
 
