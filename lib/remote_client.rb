b24fcbb496ec6c704f8dd5e796528421f290aa11
Jake Yesbeck <jake@apartmentlist.com>
2015-06-30 22:55:45 -0700

    Remove debugging

--- lib/remote_client.rb
+++ lib/remote_client.rb
@@ -7,8 +7,6 @@ module RescueGroups
   class RemoteClient
     include HTTParty
 
-    debug_output $stdout
-
     base_uri 'https://api.rescuegroups.org/http/json'
     headers 'Content-Type' => 'application/json'
     default_params apikey: RescueGroups.config.apikey
