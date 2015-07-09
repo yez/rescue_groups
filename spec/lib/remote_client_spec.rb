061aed8a59842583c2195d726d84a39f482f8fb9
Jake Yesbeck <jake@apartmentlist.com>
2015-07-08 23:01:37 -0700

    Specs for RemoteClient

---
+++ spec/lib/remote_client_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../spec_helper'
+require_relative '../../lib/remote_client'
+
+module RescueGroups
+  describe RemoteClient do
+    describe '#post_and_respond' do
+      before do
+        allow(Response).to receive(:new)
+      end
+
+      it 'calls the class post method' do
+        expect(described_class).to receive(:post)
+        subject.post_and_respond({})
+      end
+
+      context 'with no api key' do
+        before do
+          RescueGroups.configuration do |config|
+            config.apikey = nil
+          end
+        end
+
+        it 'raises an error' do
+          expect { subject.post_and_respond({}) }.to raise_error('No RescueGroups API Key set')
+        end
+      end
+    end
+  end
+end
