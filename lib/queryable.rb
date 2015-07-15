434f130b26b298284e61c6faf306dc4bca3a5201
Jake Yesbeck <jake@apartmentlist.com>
2015-07-14 22:07:24 -0700

    Private method all the things
    Also document all the things in Queryable.

--- lib/queryable.rb
+++ lib/queryable.rb
@@ -26,21 +26,12 @@ module RescueGroups
         ids_array.length == 1 ? objects.first : objects
       end
 
-      def find_body(ids)
-        {
-          objectAction: :publicView,
-          objectType:   object_type,
-          fields:       object_fields.all,
-          values:       ids.map { |i| { object_fields::FIELDS[:id] => i } }
-        }
-      end
-
       # method: where
       # purpose: find one or many objects by any supported filters
       # param: conditions - <Hash> - mutliple keyed hash containing
       #          the conditions to search against.
       #        example: { breed: 'daschund' }
-      # return: An array of found objects
+      # return: <Array> -An array of found objects
       #         If the response was not successful, an exception is thrown
       def where(conditions)
         return find(conditions[:id]) if conditions.keys == [:id]
@@ -62,6 +53,30 @@ module RescueGroups
         end
       end
 
+      private
+
+      # method: find_body
+      # purpose: Return a hash of the queryable's object specific where criteria
+      #            to be used in a find call on RescueGroups remote
+      # param: ids <Array> - list of primary keys to find remote objects by
+      # return: <Hash> - hash containing a specific configuration for performing a
+      #           a find call on RescueGroups
+      def find_body(ids)
+        {
+          objectAction: :publicView,
+          objectType:   object_type,
+          fields:       object_fields.all,
+          values:       ids.map { |i| { object_fields::FIELDS[:id] => i } }
+        }
+      end
+
+      # method: conditions_to_search_engine
+      # purpose: Given a list of unmapped filters, call helper method
+      #            conditions_to_filters and add the result to the search
+      #            new search engine that is returned
+      # param: conditions <Hash> - conditions to conduct the search on
+      # return: <Object> -  instantiated search engine
+      #           with filters added
       def conditions_to_search_engine(conditions)
         search_engine = search_engine_class.new
 
@@ -72,7 +87,12 @@ module RescueGroups
         search_engine
       end
 
-
+      # method: where_body
+      # purpose: Return a hash of the queryable's object specific where criteria
+      #            to be used in a search call on RescueGroups remote
+      # param: search_engine - <Object> - instantiated search engine
+      # return: <Hash> - hash containing a specific configuration for performing a
+      #           a search on RescueGroups
       def where_body(search_engine)
         {
           objectAction: :publicSearch,
@@ -81,8 +101,15 @@ module RescueGroups
         }
       end
 
-      private
-
+      # method: additional_request_data
+      # purpose: Alter the limit of a passed in search engine passed on an initial response
+      #            of a where request. The limit is increased by 1x per the difference of how
+      #            many results were found in the search and how many were returned.
+      # param: response - <Object> - Initial response from a where call with the count of
+      #         found rows and returned values
+      # param: search_engine - <Object> - Search engine with all added fitlers used in the
+      #          original where call
+      # return: <Hash> - Additional search results for the same filters
       def additional_request_data(response, search_engine)
         return {} if hit_request_limit?(response)
 
@@ -100,6 +127,10 @@ module RescueGroups
         end
       end
 
+      # method: hit_request_limit?
+      # purpose: Determine if the response has more data inline than the found_rows
+      # param: response - <Object> - Response from RescueGroups
+      # return: <Boolean>
       def hit_request_limit?(response)
         return true unless !response.nil? && !response['data'].nil? && !response['data'].empty?
         response['found_rows'].nil? || response['data'].keys.length >= response['found_rows']
